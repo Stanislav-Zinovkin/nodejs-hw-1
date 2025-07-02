4a3771ea40379e1a2b94e6036902811dc85abd54
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-07-02 15:09:21 +0300

    n

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = "src/db/db.json";
+import { fileURLToPath } from 'url';
+import path, {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+export const PATH_DB = path.join(__dirname, '..',  'db', 'db.json');
